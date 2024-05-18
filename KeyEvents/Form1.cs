50aa6011f4ad6a58124c7866ed888af876cb3fb9
NguyenMint <truongnm2406@gmail.com>
2024-05-19 02:05:44 +0700

    init project

---
+++ KeyEvents/Form1.cs
@@ -0,0 +1,24 @@
+﻿namespace KeyEvents
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+              this.KeyPreview = true;
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+             //labelOutput.Text += $" {e.KeyChar}";
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            labelOutput.Text += $" {e.KeyCode}";
+
+        }
+    }
+}
